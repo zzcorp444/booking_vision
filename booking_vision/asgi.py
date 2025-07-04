95080638f68b613f2f5f1b4017124a608439f8d0
ZZ-Corp <95845744+zzcorp444@users.noreply.github.com>
2025-07-05 05:18:15 +0700

    Add files via upload

--- booking_vision/asgi.py
+++ booking_vision/asgi.py
@@ -5,12 +5,26 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+
+ASGI config for booking_vision project.
 """
-
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'booking_vision.settings')
 
-application = get_asgi_application()
+from booking_vision_APP import routing
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                routing.websocket_urlpatterns
+            )
+        )
+    ),
+})
