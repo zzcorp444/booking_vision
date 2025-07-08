946827cc2d0c026d068757f078401f2b302fce00
ZZ-Corp <95845744+zzcorp444@users.noreply.github.com>
2025-07-08 09:31:35 +0700

    Add files via upload

--- booking_vision_APP/templatetags/custom_filters.py
+++ booking_vision_APP/templatetags/custom_filters.py
@@ -5,6 +5,7 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter
 def lookup(dictionary, key):
     """Look up a value in a dictionary using a key"""
@@ -12,9 +13,19 @@ def lookup(dictionary, key):
         return dictionary.get(key, [])
     return []
 
+
 @register.filter
 def date(value, format_string):
     """Format a date"""
     if hasattr(value, 'strftime'):
         return value.strftime(format_string)
     return value
+
+
+@register.filter
+def get_item(dictionary, key):
+    """
+    Custom template filter to get an item from a dictionary using a variable as key.
+    Usage: {{ mydict|get_item:item.id }}
+    """
+    return dictionary.get(key)
